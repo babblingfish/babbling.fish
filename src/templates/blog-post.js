3d1a32c8f29b04fa891831ec6a20031831a0b204
Matthew Bass <matt.l.bass@gmail.com>
2021-11-20 12:32:40 -0800

    Add reading progress bar

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,6 +5,7 @@ import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Category from "../components/category"
+import ReadingProgress from "../components/progress"
 import { rhythm, scale } from "../utils/typography"
 
 class BlogPostTemplate extends React.Component {
@@ -13,83 +14,87 @@ class BlogPostTemplate extends React.Component {
     const siteTitle = this.props.data.site.siteMetadata.title
     const { previous, next } = this.props.pageContext
     const author = this.props.data.site.siteMetadata.author
+    const target = React.createRef()
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <Seo
-          title={post.frontmatter.title}
-          description={post.frontmatter.description || post.excerpt}
-        />
-        <article>
-          <header>
-            <h1
-              style={{
-                marginTop: rhythm(1 / 2),
-                marginBottom: rhythm(1 / 4),
-                fontSize: "50px",
-              }}
-            >
-              {post.frontmatter.title}
-            </h1>
-            <small
-              style={{
-                ...scale(-1 / 5),
-                fontWeight: 300,
-                display: `block`,
-                marginBottom: rhythm(1 / 2),
-                fontFamily: `Lora`,
-                fontStyle: `italic`,
-              }}
-            >
-              By {author} · {post.frontmatter.date}
-              {` `}·
-              <Category category={post.frontmatter.category} />
-            </small>
-          </header>
-          <section dangerouslySetInnerHTML={{ __html: post.html }} />
-          <hr
-            style={{
-              marginBottom: rhythm(1),
-            }}
+      <div>
+        <ReadingProgress target={target} />
+        <Layout location={this.props.location} title={siteTitle}>
+          <Seo
+            title={post.frontmatter.title}
+            description={post.frontmatter.description || post.excerpt}
           />
-          <footer
-            style={{
-              marginTop: rhythm(1.5),
-              marginBottom: rhythm(1),
-            }}
-          >
-            <Bio />
-          </footer>
-        </article>
+          <article>
+            <header>
+              <h1
+                style={{
+                  marginTop: rhythm(1 / 2),
+                  marginBottom: rhythm(1 / 4),
+                  fontSize: "50px",
+                }}
+              >
+                {post.frontmatter.title}
+              </h1>
+              <small
+                style={{
+                  ...scale(-1 / 5),
+                  fontWeight: 300,
+                  display: `block`,
+                  marginBottom: rhythm(1 / 2),
+                  fontFamily: `Lora`,
+                  fontStyle: `italic`,
+                }}
+              >
+                By {author} · {post.frontmatter.date}
+                {` `}·
+                <Category category={post.frontmatter.category} />
+              </small>
+            </header>
+            <section dangerouslySetInnerHTML={{ __html: post.html }} ref={target} />
+            <hr
+              style={{
+                marginBottom: rhythm(1),
+              }}
+            />
+            <footer
+              style={{
+                marginTop: rhythm(1.5),
+                marginBottom: rhythm(1),
+              }}
+            >
+              <Bio />
+            </footer>
+          </article>
 
-        <nav>
-          <ul
-            style={{
-              display: `flex`,
-              flexWrap: `wrap`,
-              justifyContent: `space-between`,
-              listStyle: `none`,
-              padding: 0,
-              marginLeft: 0,
-            }}
-          >
-            <li>
-              {previous && (
-                <Link to={previous.fields.slug} rel="prev">
-                  ← {previous.frontmatter.title}
-                </Link>
-              )}
-            </li>
-            <li>
-              {next && (
-                <Link to={next.fields.slug} rel="next">
-                  {next.frontmatter.title} →
-                </Link>
-              )}
-            </li>
-          </ul>
-        </nav>
-      </Layout>
+          <nav>
+            <ul
+              style={{
+                display: `flex`,
+                flexWrap: `wrap`,
+                justifyContent: `space-between`,
+                listStyle: `none`,
+                padding: 0,
+                marginLeft: 0,
+              }}
+            >
+              <li>
+                {previous && (
+                  <Link to={previous.fields.slug} rel="prev">
+                    ← {previous.frontmatter.title}
+                  </Link>
+                )}
+              </li>
+              <li>
+                {next && (
+                  <Link to={next.fields.slug} rel="next">
+                    {next.frontmatter.title} →
+                  </Link>
+                )}
+              </li>
+            </ul>
+          </nav>
+        </Layout>
+      </div>
     )
   }
 }
